a528b1758b29e6eb04acea6cfbde2f1a375837f6
Fatih Kacar <fak9312@thi.de>
2024-12-10 19:22:31 +0100

    Revert "Neuerung der Upload Files, ohne Static File & Parameterauswahl ist wie in der PDF 'Szenarien' beschrieben"
    This reverts commit e751964ff11e2d895b56261666646f5bac7ec70f.

--- ProjektWA.py
+++ ProjektWA.py
@@ -1,21 +1,20 @@
-import os
-import streamlit as st
-from pages.startpage import start_page
 from pages.fileuploadpage import file_upload_page
 from pages.parameterselectionpage import parameter_selection_page
 from pages.resultspage import results_page
-from utils import footer
+from pages.startpage import start_page
+from utils import *
 
+# Statische Pfade und Konstanten
 DATA_DIRECTORY = "./data"
 if not os.path.exists(DATA_DIRECTORY):
     os.makedirs(DATA_DIRECTORY)
+STATIC_FILE_PATH = os.path.join(DATA_DIRECTORY, "NASA_Data.csv")
 LAST_USED_DIRECTORY = os.path.join(DATA_DIRECTORY, "last_used")
 if not os.path.exists(LAST_USED_DIRECTORY):
     os.makedirs(LAST_USED_DIRECTORY)
+WINDRAD_IMAGE_PATH = os.path.abspath("windrad.gif")
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-WINDRAD_IMAGE_PATH = os.path.join(BASE_DIR, "windrad.gif")
-
+# Funktion: Hauptlogik
 def main():
     if "page" not in st.session_state:
         st.session_state.page = "Start"
@@ -25,14 +24,15 @@ def main():
         start_page()
         footer()
     elif st.session_state.page == "Dateiauswahl":
-        file_upload_page(LAST_USED_DIRECTORY, WINDRAD_IMAGE_PATH)
+        file_upload_page(LAST_USED_DIRECTORY, WINDRAD_IMAGE_PATH, STATIC_FILE_PATH)
         footer()
     elif st.session_state.page == "Parameterauswahl":
-        parameter_selection_page()
+        parameter_selection_page(STATIC_FILE_PATH)
         footer()
     elif st.session_state.page == "Ergebnisse":
         results_page()
         footer()
+
 
 if __name__ == "__main__":
     main()
